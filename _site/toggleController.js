2e11d11a3998642a9ba7e5f3d42500d928771ace
brennontwilliams <brennonw@gmail.com>
2015-06-17 16:35:03 -0700

    Changed default markdown

---
+++ _site/toggleController.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('.toggler').click(function() {
+        //$('.toggler').find('div').slideToggle();
+        //$("p").slideToggle("slow");
+        $(this).find('div').slideToggle();
+    });
+});
