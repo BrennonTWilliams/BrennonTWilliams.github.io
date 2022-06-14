54dca7034286b86bcbe58ca0773854429bf58c3d
BrennonTWilliams <brennonw@gmail.com>
2022-06-14 13:06:04 -0500

    Created new Jekyll project to redo site from scratch

--- toggleController.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $('.toggler').click(function() {
-        //$('.toggler').find('div').slideToggle();
-        //$("p").slideToggle("slow");
-        $(this).find('div').slideToggle();
-    });
-});
